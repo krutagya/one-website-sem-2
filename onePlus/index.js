bcdbf14d8ccdc1fe50f70194e24a4104ad9c9646
krutagya <krutagya.kaneria.cg@gmail.com>
2024-12-02 15:44:46 +0530

    a) added 5 pages in which produects are in the matrix form  b) then the storage of data of user in local c)adding animation to some of the images like slide left and flip-left

--- onePlus/index.js
+++ onePlus/index.js
@@ -1,3 +1,13 @@
 function dataform(){
     window.open('form html.html','_blank');
 };
+document.getElementById('userForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const skill = document.getElementById('mobile').value;
+    const userData = { name, email, skill };
+    localStorage.setItem('user', JSON.stringify(userData));
+    alert('Form data saved successfully!');
+    this.reset();
+});
